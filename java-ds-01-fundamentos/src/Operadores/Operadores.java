cc1caf0784ea064b0865ffa97c07a5fe82d17ba1
Matheus Ferreira <matheushollifer@gmail.com>
2026-02-07 15:10:11 -0300

    Update Operadores.java
    Co-Authored-By: matheusschlange <259992272+matheusschlange@users.noreply.github.com>

--- java-ds-01-fundamentos/src/Operadores/Operadores.java
+++ java-ds-01-fundamentos/src/Operadores/Operadores.java
@@ -1,55 +1,30 @@
 package Operadores;
-import java.util.Scanner; // Importa a classe Scanner
 
 public class Operadores {
     public static void main(String[] args) {
-        // Criar a intância do scanner
-        Scanner scanner = new Scanner(System.in);
-
-        /**
-         *  Operadores de atribuição
-         *
-         *  O operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável.
-         *  Em seu uso, o operando à esquerda representa a variável para a qual desejamos atribuir o valor informado à direita.
-         *
-         */
-
-        System.out.println("\nDigite o seu nome: ");
-        String nome = scanner.nextLine();
-
-        System.out.println("\nDigite a sua idade: ");
-        int idade = scanner.nextInt();
-
-        // Limpa o buffer
-        scanner.nextLine();
+        // Operadores de atribuição
+        String nome = "Matheus Henrique de Oliveira Ferreira";
+        int idade = 20;
 
         System.out.printf("\nOlá " + nome + ". Você tem " + idade + " anos.\n");
 
-        /**
-         *  Operadores aritméticos
-         *
-         *  Os operadores aritméticos realizam as operações fundamentais da matemática entre duas variáveis e retornam o resultado.
-         *  Caso seja necessário escrever operações maiores ou mais complexas, podemos combinar esses operadores e criar expressões,
-         *  o que nos permite executar todo tipo de cálculo de forma programática.
-         *
-         */
-
+        // Operadores aritmeticos
         int soma = 2 + 2;
-        float multiplicacao = 2 * 2;
+        int subtracao = 2 - 2;
+        float multiplicacao = 2.0f * 2;
+        float divisao = 2 / 2.0f;
         byte resto = 2 % 2;
 
-        System.out.printf("\n\tA soma de 2 + 2 = %d\n\tA multiplicação de 2 * 2 = %.1f\n\tO resto da divisão de 2 %% 2 = %d%n\n", soma, multiplicacao, resto);
+        System.out.printf("\n\tA soma de 2 + 2 = %d\n\tA subtração de 2 - 2 = %d\n\tA multiplicação de 2 * 2 = %.1f\n\tA divisão de 2 / 2 = %.1f\n\tO resto da divisão de 2 %% 2 = %d%n\n", soma, subtracao, multiplicacao, divisao, resto);
 
-        /**
-         *  Operadores de incremento e decremento
-         *
-         *  Os operadores de incremento e decremento também são bastante utilizados.
-         *  Basicamente temos dois deles:++ e --, os quais podem ser declarados antes
-         *  ou depois da variável e incrementam ou decrementam em 1 o valor da variável.
-         *  
-         */
+        // Operadores de incremento e decremento
+        int x = 5;
+        System.out.printf("x = %d", x);
+        int y = x++; // y recebe 5, depois x vira 6
+        System.out.printf("\ny = %d", y);
 
-        // Fecha o scanner
-        scanner.close();
-        }
+        x = 5;
+        y = ++x; // x vira 6, depois y recebe 6
+        System.out.printf("\ny = %d", y);
+    }
 }
